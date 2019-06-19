2fa825919b58ea281136087b6445adb08c36e90a
TooFatToPogo <44614683+TooFatToPogo@users.noreply.github.com>
2019-06-19 14:47:25 +0200

    asd

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const state = {
     size: 10,
     snake: [[3,3],[3,4],[3,5]], // index 0 is row, index 1 is columns
     apple: [7,7],
+    willEat: false,
+    willCollide: false,
+    snakeDirection: 0,
     direction: 0 //0 = north, 1 = east, 2 south, 3 west
 }
 
@@ -39,22 +42,47 @@ willCollide = () => {
 
 };
 
-moveSnake = (snake, direction, size) => {
-
+moveSnake = (state) => {
+    const { time, size, snake, apple, direction, snakeDirection, willEat } = state;
     switch(direction) {
-        case 0: snake.unshift([(snake[0][0] == 0 ? size - 1 : snake[0][0] - 1), snake[0][1]]); break
-        case 1: snake.unshift([snake[0][0], (snake[0][1] == size -1 ? 0 : snake[0][1] + 1)]); break
-        case 2: snake.unshift([(snake[0][0] == size -1 ? 0 : snake[0][0] + 1), snake[0][1]]); break
-        case 3: snake.unshift([snake[0][0], (snake[0][1] == 0 ? size - 1 : snake[0][1] - 1)]); break
+        case 0: if(snakeDirection == 2) {
+            snake.unshift([(snake[0][0] == size -1 ? 0 : snake[0][0] + 1), snake[0][1]]);
+        } else {
+            snake.unshift([(snake[0][0] == 0 ? size - 1 : snake[0][0] - 1), snake[0][1]]);
+            state.snakeDirection = direction;
+        }; break
+        case 1: if(snakeDirection == 3) {
+            state.snake.unshift([snake[0][0], (snake[0][1] == 0 ? size - 1 : snake[0][1] - 1)]);
+        } else {
+            snake.unshift([snake[0][0], (snake[0][1] == size -1 ? 0 : snake[0][1] + 1)]);
+            state.snakeDirection = direction;
+        }; break
+        case 2: if(snakeDirection == 0) {
+            snake.unshift([(snake[0][0] == 0 ? size - 1 : snake[0][0] - 1), snake[0][1]]);
+        } else {
+            snake.unshift([(snake[0][0] == size -1 ? 0 : snake[0][0] + 1), snake[0][1]]);
+            state.snakeDirection = direction;
+        }; break
+        case 3: if(snakeDirection == 1) {
+            snake.unshift([snake[0][0], (snake[0][1] == size -1 ? 0 : snake[0][1] + 1)]);
+        } else {
+            snake.unshift([snake[0][0], (snake[0][1] == 0 ? size - 1 : snake[0][1] - 1)]);
+            state.snakeDirection = direction;
+        };  break
     }
-    snake.pop();
+    if (snake[0][0] == apple[0] && snake[0][1] == apple[1]) {
+        willEat = true;
+    } else {
+        snake.pop();
+    }
 }
 
 step = t1 => {
-    const { time, size, snake, apple, direction } = state;
-    if( t1 - time > 320 ) {
+    const { time, size, snake, apple, direction, snakeDirection } = state;
+    if( t1 - time > 160 ) {
+        moveSnake( snake, direction, size, snakeDirection );
+        
         draw( snake, apple, size );
-        moveSnake(snake, direction, size);
         
         state.time = t1;
     }
