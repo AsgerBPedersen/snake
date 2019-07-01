c87dca554a6187d8da3ddf6246a8b8fcd64a1e91
unknown <inzolin2@gmail.com>
2019-07-01 16:52:50 +0200

    Astar works :)

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//const readline = require('readline');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const state = {
     time: 0,
-    size: 20,
+    size: 50,
     snake: [[3,1],[3,0]], // index 0 is row, index 1 is columns
     apple: [1,1],
     score: 0,
@@ -19,6 +18,8 @@ const state = {
 
 const xy = c => Math.round(c * canvas.width / state.size);
 const pointEq = (x, y) => (x[0] == y[0] && x[1] == y[1])? true : false;
+const pointObjEq = (x, y) => (x.x == y.x && x.y == y.y)? true : false;
+const calcDistance = (x1,x2,y1,y2) => (x2 - x1 > (0.5 * state.size)? state.size - (x2 - x1) : x2 - x1) + (y2 - y1 > (0.5 * state.size)? state.size - (y2 - y1) : y2 - y1);
 
 
 const moves = (node, allNodes, bool) => {
@@ -53,7 +54,7 @@ const moves = (node, allNodes, bool) => {
     return result;
 }
 
-pathFinder = ({snake, apple, size}) => {
+const pathFinder = ({snake, apple, size}) => {
     
     let nodes = [];
 
@@ -104,44 +105,152 @@ pathFinder = ({snake, apple, size}) => {
     return path;
 }
 
+const movesWithDistance = (node, allNodes) => {
+    const {size} = state;
+    const directions = [[-1, 0], [0, 1], [1,0], [0, -1]];  //0 = north, 1 = east, 2 south, 3 west
+    let result = [];
+    directions.forEach(d => {
+        let newEdge;
+        if(node.x + d[0] == -1) {
+            newEdge = {
+                x: size - 1,
+                y: node.y,
+                distance: 0
+            };
+        } else if(node.x + d[0] == size) {
+            newEdge = {
+                x: 0,
+                y: node.y,
+                distance: 0
+            } 
+        } else if(node.y + d[1] == -1) {
+            newEdge = {
+                x: node.x,
+                y: size -1,
+                distance: 0
+            }
+        } else if(node.y + d[1] == size) {
+            newEdge = {
+                x: node.x,
+                y: 0,
+                distance: 0
+            }
+        } else {
+            newEdge = {
+                x: node.x + d[0],
+                y: node.y + d[1],
+                distance: 0
+            }
+        }
 
-draw = ({ snake, apple, size, score }) => {
+        const edge = allNodes.find(n => pointObjEq(n, newEdge));
+        
+        if(edge) {
+            if(edge.x > state.apple[0]) {
+                if(edge.y > state.apple[1]) {
+                    edge.distance = calcDistance(state.apple[0], edge.x, state.apple[1], edge.y);
+                } else {
+                    edge.distance = calcDistance(state.apple[0], edge.x, edge.y, state.apple[1]);
+                }
+            } else {
+                if(edge.y > state.apple[1]) {
+                    edge.distance = calcDistance(edge.x, state.apple[0], state.apple[1], edge.y);
+                } else {
+                    edge.distance = calcDistance(edge.x, state.apple[0], edge.y, state.apple[1]);
+                }
+            }
+            result.push(edge)
+        }
+    });
+    
+    return result;
+}
+
+const pathFinderAStar = ({snake, apple, size}) => {
+    
+    let nodes = [];
+
+    for(x = 0; x < size; x++) {
+        for (y = 0; y < size; y++) {
+            if(!snake.some(s => s[0] == x && s[1] == y)) {
+                nodes.push({
+                    x: x,
+                    y: y,
+                    distance: 0
+                });
+            }
+        }
+    }
+
+    const end = {
+        x: snake[0][0],
+        y: snake[0][1],
+        distance: 9999
+    };
+    let frontier = [];
+    frontier.unshift(end);
+    let visited = new Map();
+    visited.set(frontier[0], null)
+    let current;
+    while(frontier.length != 0) {
+        
+        current = frontier.shift();
+        console.log(current);
+        if(pointEq([current.x, current.y], apple)) {
+            break;
+        }
+        const newEdges = movesWithDistance(current, nodes);
+        
+        newEdges.forEach(e => {
+            if(!visited.has(e)) {
+                frontier.push(e);
+                frontier.sort((a,b) => a.distance - b.distance);
+                visited.set(e, current);
+                if(state.calcPath) {
+                    state.frontier.push(frontier.map(f => f))
+                }
+            }
+        });
+        
+    }
+    
+    let path = [];
+    while(current != end) {
+        path.unshift([current.x, current.y]);
+        current = visited.get(current);
+    }
+    if(path.length == 0) {
+        const nextMoves = moves(snake[0], snake, false);
+        
+        if(nextMoves.length == 0) {
+            willCollide()
+        } else {
+            path.push(nextMoves[Math.floor(Math.random()*nextMoves.length)])
+        }
+    }
+    console.log(path);
+    return path;
+}
+
+
+const draw = ({ snake, apple, size, score }) => {
 
     ctx.fillStyle = '#232323';
     ctx.fillRect(0,0,canvas.width, canvas.height);
 
     ctx.fillStyle = 'rgb(0,200,50)';
     snake.map(s => ctx.fillRect(xy(s[1]), xy(s[0]), xy(1), xy(1)));
+
+    ctx.fillStyle = '#232323';
+    ctx.fillRect(xy(snake[0][1]) + xy(0.4), xy(snake[0][0]) + xy(0.15), xy(0.2),xy(0.2))
+    ctx.fillRect(xy(snake[0][1]) + xy(0.4), xy(snake[0][0]) + xy(0.65), xy(0.2),xy(0.2))
 
     ctx.fillStyle = 'rgb(255,50,0)';
     ctx.fillRect(xy(apple[1]), xy(apple[0]), xy(1), xy(1));
-    // let map = "";
-
-    // for (let i = 0; i < size; i++) {
-
-    //     let line = Array.from(" ".repeat(size) + "\n");
-        
-    //     if(snake.some(e => e[0] == i)) 
-    //         {
-    //             const snakes = snake.filter(e => { if (e[0] == i) return e });
-                
-    //             snakes.forEach(e => line[e[1]] = "x");
-                
-    //         }
-    //     if(apple[0] == i) {
-    //         line[apple[1]] = "o"
-    //     }
-    //         for(let y = 0; y < size; y++) {
-    //             line[y] += " ";
-    //         }
-    //         map += line.join('');
-    //     }
-    // console.clear();
-    // console.log(map);
-    // console.log(score)
+    
 };
 
-drawFrontier = ({ snake, apple, size, score }, frontier) => {
+const drawFrontier = ({ snake, apple, size, score }, frontier) => {
 
     ctx.fillStyle = '#232323';
     ctx.fillRect(0,0,canvas.width, canvas.height);
@@ -149,9 +258,13 @@ drawFrontier = ({ snake, apple, size, score }, frontier) => {
     ctx.fillStyle = 'rgb(0,200,50)';
     snake.map(s => ctx.fillRect(xy(s[1]), xy(s[0]), xy(1), xy(1)));
 
+    ctx.fillStyle = '#232323';
+    ctx.fillRect(xy(snake[0][1]) + xy(0.4), xy(snake[0][0]) + xy(0.15), xy(0.2),xy(0.2))
+    ctx.fillRect(xy(snake[0][1]) + xy(0.4), xy(snake[0][0]) + xy(0.65), xy(0.2),xy(0.2))
+
     ctx.fillStyle = '#fefefe';
     const newFrontier = frontier.shift();
-    newFrontier.map(f => ctx.fillRect(xy(f[1]), xy(f[0]), xy(1), xy(1)));
+    newFrontier.map(f => ctx.fillRect(xy(f.y), xy(f.x), xy(1), xy(1)));
     
     ctx.fillStyle = 'rgb(255,50,0)';
     ctx.fillRect(xy(apple[1]), xy(apple[0]), xy(1), xy(1));
@@ -162,7 +275,7 @@ drawFrontier = ({ snake, apple, size, score }, frontier) => {
     }
 };
 
-willEat = ({ size, snake, apple, score }) => {
+const willEat = ({ size, snake, apple, score }) => {
    state.score += 10; 
    state.apple = [Math.floor(Math.random()* size), Math.floor(Math.random()* size)];
    while(snake.some(e => e[0] == state.apple[0] && e[1] == state.apple[1])) {
@@ -171,82 +284,13 @@ willEat = ({ size, snake, apple, score }) => {
    
 };
 
-willCollide = () => {
+const willCollide = () => {
     console.log("game over");
     process.exit()
 };
 
-moveSnake = ({ path, size, snake, apple, direction, snakeDirection }) => {
+const moveSnake = ({ path, size, snake, apple, direction, snakeDirection }) => {
 
-    // switch(direction) {
-    //     case 0: if(snakeDirection == 2) {
-    //         const head = [(snake[0][0] == size -1 ? 0 : snake[0][0] + 1), snake[0][1]];
-    //         if(snake.some(e => e[0] == head[0] && head[1] == e[1])) {
-    //             willCollide()
-    //         }
-    //         snake.unshift(head);
-    //     } else {
-    //         const head = [(snake[0][0] == 0 ? size - 1 : snake[0][0] - 1), snake[0][1]];
-    //         if(snake.some(e => e[0] == head[0] && head[1] == e[1])) {
-    //             willCollide()
-    //         }
-    //         snake.unshift(head);
-    //         state.snakeDirection = direction;
-    //     }; break
-    //     case 1: if(snakeDirection == 3) {
-    //         const head = [snake[0][0], (snake[0][1] == 0 ? size - 1 : snake[0][1] - 1)];
-    //         if(snake.some(e => e[0] == head[0] && head[1] == e[1])) {
-    //             willCollide()
-    //         }
-    //         snake.unshift(head);
-    //     } else {
-    //         const head = [snake[0][0], (snake[0][1] == size -1 ? 0 : snake[0][1] + 1)];
-    //         if(snake.some(e => e[0] == head[0] && head[1] == e[1])) {
-    //             willCollide()
-    //         }
-    //         snake.unshift(head);
-    //         state.snakeDirection = direction;
-    //     }; break
-    //     case 2: if(snakeDirection == 0) {
-    //         const head = [(snake[0][0] == 0 ? size - 1 : snake[0][0] - 1), snake[0][1]];
-    //         if(snake.some(e => e[0] == head[0] && head[1] == e[1])) {
-    //             willCollide()
-    //         }
-    //         snake.unshift(head);
-    //     } else {
-    //         const head = [(snake[0][0] == size -1 ? 0 : snake[0][0] + 1), snake[0][1]];
-    //         if(snake.some(e => e[0] == head[0] && head[1] == e[1])) {
-    //             willCollide()
-    //         }
-    //         snake.unshift(head);
-    //         state.snakeDirection = direction;
-    //     }; break
-    //     case 3: if(snakeDirection == 1) {
-    //         const head = [snake[0][0], (snake[0][1] == size -1 ? 0 : snake[0][1] + 1)];
-    //         if(snake.some(e => e[0] == head[0] && head[1] == e[1])) {
-    //             willCollide()
-    //         }
-    //         snake.unshift(head);
-    //     } else {
-    //         const head = [snake[0][0], (snake[0][1] == 0 ? size - 1 : snake[0][1] - 1)];
-    //         if(snake.some(e => e[0] == head[0] && head[1] == e[1])) {
-    //             willCollide()
-    //         }
-    //         snake.unshift(head);
-    //         state.snakeDirection = direction;
-    //     };  break
-    // }
-    // let nextMove;
-    // if (path[0] == undefined) {
-    //    const nextMoves = moves(snake[0], snake, false);
-    //    console.log(nextMoves);
-    //    if(nextMoves[0] != undefined) {
-    //        nextMove = nextMoves[0];
-    //    } else {
-    //     willCollide()
-    //    }
-    // } else {
-    // }
     const nextMove = path.shift();
     if(snake.some(e => pointEq(e, nextMove))) {
         willCollide()
@@ -259,15 +303,15 @@ moveSnake = ({ path, size, snake, apple, direction, snakeDirection }) => {
     } else {
         snake.pop();
     }
-    state.path = pathFinder(state);
+    state.path = pathFinderAStar(state);
 }
 
 
-step = t1 => t2 => {
+const step = t1 => t2 => {
     if(state.calcPath) {
         
-        if( t2 - t1 > 1 ) {
-            console.log(state.frontier);
+        if( t2 - t1 > 20 ) {
+            
             drawFrontier(state, state.frontier);
             window.requestAnimationFrame(step(t2))
         }
@@ -275,9 +319,10 @@ step = t1 => t2 => {
             window.requestAnimationFrame(step(t1))
         }
     } else {
-        if( t2 - t1 > 10 ) {
+        if( t2 - t1 > 20 ) {
             moveSnake(state);
             draw(state);
+            console.log(state);
             window.requestAnimationFrame(step(t2))
         }
         else {
@@ -287,20 +332,8 @@ step = t1 => t2 => {
 };
 
 
-// readline.emitKeypressEvents(process.stdin);
-// process.stdin.setRawMode(true);
-// process.stdin.on('keypress', (str, key) => {
-//     if (key.ctrl && key.name === 'c') process.exit()
-//     switch (key.name.toUpperCase()) {
-//         case 'UP': state.direction = 0; break
-//         case 'RIGHT': state.direction = 1; break
-//         case 'DOWN': state.direction = 2; break
-//         case 'LEFT': state.direction = 3; break
-//     }
-// })
-
-state.path = pathFinder(state);
+state.path = pathFinderAStar(state);
 
 draw(state)
 window.requestAnimationFrame(step(0))
-//setInterval(() => step(Date.now()), 40)
+
